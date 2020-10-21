bae79e4dc85242c4d89e020a49195f8202be2472
Christian Leitgeb <christian.leitgeb@leica-geosystems.com>
2020-10-21 09:29:30 +0200

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+let newAr = new Array();
+
+fs.createReadStream('C:\\Users\\LEIT\\Desktop\\ARTIKELIMPORT4.csv')
+    .pipe(csv({separator: ';'}))
+    .on('data', (row) => {
+        //console.log(row.size);
+
+        singleSizeAr = row.size.split(',')
+        singleSizeAr.forEach(size => {
+            let t = row;
+            t.size = size;
+
+
+            // console.log(t);
+            newAr.push(t.size);
+
+        })
+        singleSizeAr = []
+    })
+    .on('end', () => {
+        //console.log(newAr);
+        console.log('CSV file successfully processed');
+    });
